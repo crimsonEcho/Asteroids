3d0e425af7f41cc138391c3496ea9ae267d22548
crimsonEcho <status.effective@gmail.com>
2025-10-13 22:10:13 -0500

    add circleshape.py and player.py - modify main.py to include player draw.

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 from constants import *
+from player import Player
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}") 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    for x in drawable:
+        x.draw(screen)
+
+    updatable.update(dt) 
+     
     while True:
-        screen.fill("black", rect=None, special_flags= 0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
+            
 if __name__ == "__main__":
     main()
-    pygame.display.flip()
+
 
